fe4469f0f89bc6f74eea07df6df100251baa2609
Shivansh Patel <56928025+shivanshme@users.noreply.github.com>
2025-03-30 12:53:29 +0000

    addded copilot requested changes

--- JsonInNode/index.js
+++ JsonInNode/index.js
@@ -1,29 +1,35 @@
 //Javascript object notation(JSON)
 //used when data is sent from server
 import { writeFile, readFile } from "fs";
+
 const biodata = {
     name: "Shivansh ",
     age: 23,
-    lastname:"Patel"
-}
+    lastname: "Patel"
+};
+
 console.log(biodata.lastname);
 
-const jsonData = JSON.stringify(biodata);     //to convert object into json
+const jsonData = JSON.stringify(biodata); //to convert object into json
 console.log(jsonData);
 
 writeFile("json1.json", jsonData, (err) => {
-    console.log("done");
+    if (err) {
+        console.error("Error writing file:", err);
+        return;
+    }
+    console.log("File written successfully");
+
+    // Read the file after writing
+    readFile("json1.json", "utf-8", (err, data) => {
+        if (err) {
+            console.error("Error reading file:", err);
+            return;
+        }
+        const original = JSON.parse(data);
+        console.log(original.age);
+    });
 });
-readFile("json1.json", "utf-8", (err, data) => {
-    //console.log(data);
-    const orginal = JSON.parse(data);
-    console.log(orginal.age);
-});
-
-
-
-// const orgData = JSON.parse(jsonData);           //converted back into object
-// console.log(orgData.age);
 
 
 
